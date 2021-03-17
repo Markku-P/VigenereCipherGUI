98405b43485bc06a9c2cd7f35ccf5eca72eb317a
M2430 <M2430@student.jamk.fi>
2021-03-17 16:57:34 +0200

    Add encrypt and decrypt buttons

--- package/ui/top_menu.py
+++ package/ui/top_menu.py
@@ -13,18 +13,28 @@ class TopMenu(QWidget):
     def __init__(self):
         super().__init__()
         # Create layouts
-        self.setFixedHeight(100)
+        self.setFixedHeight(150)
         self.layout = QHBoxLayout()
         self.layout.setContentsMargins(10,0,10,5)
         self.layout.setSpacing(0)
         
         self.group_box_layout = QVBoxLayout()
-        self.group_box_layout.setContentsMargins(10,0,0,0)
+        self.group_box_layout.setContentsMargins(10,0,0,10)
         self.group_box_layout.setSpacing(0)
 
-        # Create groupbox
+        self.group_box_layout_mode = QVBoxLayout()
+        self.group_box_layout_mode.setContentsMargins(10,5,10,5)
+        self.group_box_layout_mode.setSpacing(0)
+
+
+        # Create groupboxs
         self.group_box = QGroupBox("Settings")
         self.group_box.setLayout(self.group_box_layout)
+
+        self.group_box_mode = QGroupBox("Mode")
+        self.group_box_mode.setStyleSheet("QGroupBox { font-size: 14px; } ")
+        self.group_box_mode.setMaximumWidth(160)
+        self.group_box_mode.setLayout(self.group_box_layout_mode)
 
         # Create buttons
         self.button_encrypt = QPushButton("Encrypt")
@@ -37,9 +47,11 @@ class TopMenu(QWidget):
         self.button_decrypt.setMaximumHeight(26)
         self.button_decrypt.clicked.connect(self.decrypt_pressed)
 
-        # Add widget to layout
-        self.group_box_layout.addWidget(self.button_encrypt)
-        self.group_box_layout.addWidget(self.button_decrypt)
+        # Add widget to layouts
+        self.group_box_layout_mode.addWidget(self.button_encrypt)
+        self.group_box_layout_mode.addWidget(self.button_decrypt)
+        self.group_box_layout.addWidget(self.group_box_mode)
+
         self.layout.addWidget(self.group_box)
 
         # Set default mode to encrypt
